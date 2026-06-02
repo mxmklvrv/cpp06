f07f32e59d439340886e629a653b87c3b041cc83
Maxim Kleverov <maximkleverov@icloud.com>
2026-06-02 14:35:30 +0300

    done 02

---
+++ ex02/main.cpp
@@ -0,0 +1,89 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include "iostream"
+#include "ctime"
+#include "cstdlib"
+
+
+
+
+Base* generate(void){
+    switch (std::rand() % 3)
+    {
+    case 0:
+        return new A();
+        break;
+    case 1:
+        return new B();
+        break;
+    default:
+        return new C();
+        break;
+    }
+}
+
+// what derived type is behind base pointer
+// param is pointer
+// if fails, returns nullptr
+// dynamic cast checks at the runtime
+void identify(Base* p){
+    if(dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+    else
+        std::cout << "Unexpected type" << std::endl;
+}
+
+// what derived type is behind base ref
+// param is ref
+// if fails, throws 
+// ref cannot be null
+//
+void identify(Base& p){
+    try{
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return;
+    }
+    catch(...){
+    }
+    try{
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return;
+    }
+    catch(...){
+    }
+     try{
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return;
+    }
+    catch(...){
+    }
+    std::cout << "Unexpected type" << std::endl;
+}
+
+
+
+int main(){
+std::srand(std::time(0));
+
+Base* obj = generate();
+
+std::cout << "Pointer check: ";
+identify(obj);
+std::cout << std::endl;
+
+std::cout << "Reference check: ";
+identify(*obj);
+std::cout << std::endl;
+
+delete obj;
+
+}
